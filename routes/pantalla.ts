47867faf186820d464dd1a3ab6b6e6c45768c171
Bastián Núñez <bastinuez@hotmail.com>
2024-06-09 17:07:12 -0400

    feat(mensajes-pantallas): verificaciones de form

--- routes/pantalla.ts
+++ routes/pantalla.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { PrismaClient } from '@prisma/client';
 import Joi from 'joi';
 import { v4 as uuidv4 } from 'uuid';
-import  {publishMessage, scheduleMessage}  from '../utils/mqtt';
+import  {publishMessage, scheduleMessage, scheduleMessageConTiempo}  from '../utils/mqtt';
 import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 
@@ -15,20 +15,64 @@ const schemaRegistrarPantalla = Joi.object({
 });
 
 const schemaActualizarMensaje = Joi.object({
-    id: Joi.number().required(),
-    mensaje: Joi.string().required().min(3).max(50),
-    animacion: Joi.string().required()
+    id: Joi.number().required().messages({
+        'number.base': 'El id debe ser un número.',
+        'number.positive': 'El id debe ser un número positivo.',
+        'any.required': 'El campo id es requerido.'
+    }),
+    mensaje: Joi.string().required().min(3).max(50).pattern(/^[^&]*$/, 'no &').messages({
+        'any.required': 'mensaje, campo que registra el mensaje es obligatorio',
+        'string.empty': 'mensaje, campo que registra el mensaje, no puede estar vacio',
+        'string.min': 'mensaje, campo que registra el mensaje, debe tener un largo mínimo de 3 caracteres',
+        'string.max': 'mensaje, campo que registra el mensaje, debe tener un largo máximo de 50 caracteres',
+        'string.pattern.base': 'mensaje, campo que registra el mensaje, no puede contener el carácter "&"',
+    }),
+    animacion: Joi.number().required().min(0).max(10).messages({
+        'any.required': 'animacion, campo que registra el tipo de animación, es obligatorio',
+        'number.min': 'animacion, campo que registra el tipo de animación, debe ser entre 0 y 10',
+        'number.max': 'animacion, campo que registra el tipo de animación, debe ser entre 0 y 10',
+    })
 });
 const schemaMensajeProgramado = Joi.object({
-    id: Joi.number().required(),
-    mensaje: Joi.string().required().min(3).max(50),
-    animacion: Joi.string().required(),
-    fecha_inicio: Joi.string(),
-    dias: Joi.string().required(),
-    hora_inicio:Joi.string(),
-    hora_fin:Joi.string(),
-    fecha_fin:Joi.string(),
-    tiempo_actividad:Joi.string()
+    id: Joi.number().required().messages({
+        'number.base': 'El id debe ser un número.',
+        'number.positive': 'El id debe ser un número positivo.',
+        'any.required': 'El campo id es requerido.'
+    }),
+    mensaje: Joi.string().required().min(3).max(50).pattern(/^[^&]*$/, 'no &').messages({
+        'any.required': 'mensaje, campo que registra el mensaje es obligatorio',
+        'string.empty': 'mensaje, campo que registra el mensaje, no puede estar vacio',
+        'string.min': 'mensaje, campo que registra el mensaje, debe tener un largo mínimo de 3 caracteres',
+        'string.max': 'mensaje, campo que registra el mensaje, debe tener un largo máximo de 50 caracteres',
+        'string.pattern.base': 'mensaje, campo que registra el mensaje, no puede contener el carácter "&"',
+    }),
+    animacion: Joi.number().required().min(0).max(10).messages({
+        'any.required': 'animacion, campo que registra el tipo de animación, es obligatorio',
+        'number.min': 'animacion, campo que registra el tipo de animación, debe ser entre 0 y 10',
+        'number.max': 'animacion, campo que registra el tipo de animación, debe ser entre 0 y 10',
+    }),
+    dias: Joi.string().required().messages({
+        'any.required': 'dias, campo que registra los dias del mensaje programado es obligatorio',
+        'string.empty': 'dias, campo que registra los dias del mensaje programado, no puede estar vacio'
+    }),
+    fecha_inicio: Joi.string().allow('').pattern(/^\d{4}-\d{2}-\d{2}$/, 'date').messages({
+      'string.pattern.base': 'La fecha de inicio debe estar en el formato yyyy-mm-dd.'
+    }),
+    hora_inicio:Joi.string().allow('').pattern(/^(?:[01]\d|2[0-3]):[0-5]\d$/, 'time').messages({
+      'string.pattern.base': 'La hora de inicio debe estar en el formato HH:MM.'
+    }),
+    hora_fin:Joi.string().allow('').pattern(/^(?:[01]\d|2[0-3]):[0-5]\d$/, 'time').messages({
+        'string.pattern.base': 'La hora de inicio debe estar en el formato HH:MM.'
+    }),
+    fecha_fin:Joi.string().allow('').pattern(/^\d{4}-\d{2}-\d{2}$/, 'date').messages({
+        'string.pattern.base': 'La fecha de inicio debe estar en el formato yyyy-mm-dd.'
+    }),
+    tiempo_actividad:Joi.number().positive().min(5).max(50400).messages({
+      'number.base': 'El tiempo debe ser un número.',
+      'number.positive': 'El tiempo debe ser un número positivo.',
+      'number.min': 'El tiempo debe superar los 5 segundos.',
+      'number.max': 'El tiempo no debe superar los 50400 segundos.'
+    })
 });
 
 
@@ -85,9 +129,13 @@ router.patch('/enviar-mensaje', async (req, res) => {
             .end();
     }
 
+    const animacion: string = req.body.animacion;
     const pantallaActualizada = await prisma.pantalla.update({
         where: { id: id },
-        data: { mensajeActual: mensaje },
+        data: { 
+            mensajeActual: mensaje+"&"+String(animacion),
+            mensajeDefecto: mensaje+"&"+String(animacion)
+        },
     });
     
     if (pantallaActualizada){
@@ -102,7 +150,6 @@ router.patch('/enviar-mensaje', async (req, res) => {
 
 router.patch('/enviar-mensaje-programado', async (req, res) => {
     const { error } = schemaMensajeProgramado.validate(req.body);
-
     if (error) {
         return res.status(400).set('x-mensaje', error.details[0].message).end();
     }
@@ -113,34 +160,76 @@ router.patch('/enviar-mensaje-programado', async (req, res) => {
     const pantalla = await prisma.pantalla.findUnique({
         where: { id: id },
     });
-
     if (!pantalla) {
         return res
             .status(404)
             .set('x-mensaje', 'Pantalla no encontrada')
             .end();
     }
-    const { dias, fecha_inicio, hora_inicio, hora_fin, animacion, fechaFin } = req.body;
+    const { dias, fecha_inicio, hora_inicio, hora_fin, animacion, fecha_fin, tiempo_actividad } = req.body;
     const jobId = uuidv4();
+
+    //si tiene fecha inicio puede tener hora de inicio, si tiene hora inicio si o si debe tener fecha inicio
+    //si tiene fecha termino puede tener hor de fin, si tiene hora fin si o si debe tener fecha fin
+
+    if (fecha_fin && !fecha_inicio){
+        //no tiene sentido tener fecha de termino y no de inicio
+        return res.status(411)
+        .set('x-mensaje', 'No tiene sentido tener hora de termino y no fecha termino')
+        .end();
+    }
+    if (hora_inicio && !fecha_inicio){
+        //no tiene sentido no tener fecha de inicio y si hora
+        return res.status(410)
+            .set('x-mensaje', 'No tiene sentido tener hora de inicio y no fecha inicio')
+            .end();
+    }
+    if (hora_fin && !fecha_fin){
+        //no tiene sentido no tener fecha de inicio y si hora
+        return res.status(410)
+            .set('x-mensaje', 'No tiene sentido tener hora de termino y no fecha termino')
+            .end();
+    }
+    
+    
     //fechas
     const fechaInicioDate = new Date(fecha_inicio);
     const inicio_year=fechaInicioDate.getFullYear();
-    const inicio_month=fechaInicioDate.getMonth()+1;
+    const inicio_month=fechaInicioDate.getMonth();
     const inicio_day=fechaInicioDate.getDate()+1;
-    const fechaFinDate = fechaFin ? new Date(fechaFin) : null;
+
+    const fechaFinDate = new Date(fecha_fin);
+    const fin_year=fechaFinDate.getFullYear();
+    const fin_month=fechaFinDate.getMonth();
+    const fin_day=fechaFinDate.getDate()+1;
+
     //horas
-    const hora_inicio_array = hora_inicio.split(":");
-    const inicio_hora = hora_inicio_array[0]
-    const inicio_minuto = hora_inicio_array[1]
-    const trans_date_hora_inicio = new Date(inicio_year,inicio_month,inicio_day,inicio_hora,inicio_minuto);
-    // console.log("hora:",trans_date_hora_inicio.getHours())
-    // console.log("minuto:",trans_date_hora_inicio.getMinutes())
-
-    // console.log("fecha_inicio_date:",fechaInicioDate)
-    // console.log("hora_inicio:",trans_date_hora_inicio)
-
-    scheduleMessage(jobId,dias,trans_date_hora_inicio,trans_date_hora_inicio,hora_fin,pantalla.nombre,mensaje,animacion,fechaFinDate)
+    const hora_inicio_array = hora_inicio? hora_inicio.split(":") : '';
+    const inicio_hora = hora_inicio? hora_inicio_array[0] : '';
+    const inicio_minuto = hora_inicio? hora_inicio_array[1] : '';
+    const trans_date_hora_inicio = hora_inicio? new Date(inicio_year,inicio_month,inicio_day,inicio_hora,inicio_minuto) : '';
     
+    const hora_fin_array = hora_fin? hora_fin.split(":"): '';
+    const fin_hora = hora_fin? hora_fin_array[0] : ''
+    const fin_minuto = hora_fin? hora_fin_array[1] : ''
+    const trans_date_hora_fin = hora_fin? new Date(fin_year,fin_month,fin_day,fin_hora,fin_minuto) : '';
+    
+    if (tiempo_actividad){
+        //esta funcion solo permite fecha y hora inicio por unos segundos/minutos/horas definidas, no tiene una fecha como tal
+        scheduleMessageConTiempo(jobId,dias,trans_date_hora_inicio,trans_date_hora_inicio,pantalla.nombre,
+            mensaje,animacion,Number(tiempo_actividad),pantalla.mensajeDefecto?pantalla.mensajeDefecto:'')
+    }else{
+        //esta funcion permite setear la fecha,hora inicio y fecha,hora fin de un mensaje
+        scheduleMessage(jobId,dias,fechaInicioDate,trans_date_hora_inicio,trans_date_hora_fin,pantalla.nombre
+            ,mensaje,animacion,fechaFinDate,pantalla.mensajeDefecto?pantalla.mensajeDefecto:'')
+    }
+
+    const pantallaActualizada = await prisma.pantalla.update({
+        where: { id: id },
+        data: { 
+            mensajeActual: mensaje+"&"+String(animacion)
+        },
+    });
 
     return res
         .status(200)
@@ -158,7 +247,7 @@ router.get('', async (req, res) => {
     }
 
     return res
-        .status(404)
+        .status(200)
         .set('x-mensaje', 'Información de las pantallas')
         .send(pantallas)
 });
--- utils/mqtt.ts
+++ utils/mqtt.ts
@@ -15,50 +15,111 @@ export const publishMessage = (pantalla:string, mensaje:string) => {
   client.publish(pantalla, mensaje);
 };
 
-export const scheduleMessage = (jobId:any, dias:any, fechaInicio:any, de:any, a:any, pantalla:any, mensaje:any, animacion:any, fechaFin:any) => {
-    const jobOptions = {
-        dayOfWeek: dias,
-        hour: fechaInicio.getHours(),
-        minute: fechaInicio.getMinutes(),
-        second: fechaInicio.getSeconds(),
-    };
-    console.log(`Iniciando la programación del mensaje con jobId: ${jobId}`);
-    console.log(`Configuración de la tarea principal: ${JSON.stringify(jobOptions)}`);
+export const scheduleMessage = (jobId:any, dias:string, fechaInicio:any, fecha_hora_inicio:any, 
+    fecha_hora_fin:any, pantalla:string, mensaje:string, animacion:number, fechaFin:any,mensaje_actual:string|null) => {
 
-    // schedule.scheduleJob(jobId, jobOptions, () => {
-    //     console.log(`Ejecutando la tarea programada con jobId: ${jobId}`);
-    //     publishMessage(pantalla, mensaje);
-    // });
-
-    const job = schedule.scheduleJob('* * * * *', function(){
-        console.log(`Ejecutando la tarea programada con jobId: ${jobId}`);
-        publishMessage(pantalla, mensaje);
-    });
-
-    if (de && a) {
-        const quitarOptions = {
-            dayOfWeek: dias,
-            hour: a.getHours(),
-            minute: a.getMinutes(),
-            second: a.getSeconds(),
-        };
-        console.log(`Configuración de la tarea para quitar el mensaje: ${JSON.stringify(quitarOptions)}`);
+    const new_msg = mensaje+"&"+String(animacion)
         
-        schedule.scheduleJob(`${jobId}/quitar`, quitarOptions, () => {
-            console.log(`Ejecutando la tarea de quitar el mensaje con jobId: ${jobId}/quitar`);
-            // Logic to remove the message
-        });
+    //SE PUEDE ELEGIR ENTRE TODOS LOS DIAS, LUNES A VIERNES, SÁBADO Y DOMINGO O SELECCIONAR LOS DIAS
+    let dias_fin = ''
+    if (dias.includes('Todos los días')){
+        //se setea todos los días a las 8 de la mañana
+        dias_fin='*'
+    }else if (dias.includes('Lunes a Viernes')){
+        //se setea de lunes a viernes a las 8 de la mañana
+        dias_fin='1-5'
+    }else if (dias.includes('Sábado y Domingo')){
+        //se setea sabado y domingo a las 8 de la mañana
+        dias_fin='0,6'
+    }else{
+        let dias:string = ''
+        dias += dias.includes('Lunes') ? dias += "1" : '';
+        dias += dias.includes('Martes') ? dias += ",2" : '';
+        dias += dias.includes('Miércoles') ? dias += ",3" : '';
+        dias += dias.includes('Jueves') ? dias += ",4" : '';
+        dias += dias.includes('Viernes') ? dias += ",5" : '';
+        dias += dias.includes('Sábado') ? dias += ",6" : '';
+        dias += dias.includes('Domingo') ? dias += ",0" : '';
+        dias_fin = dias[0] == ',' ? dias.substring(1,dias.length) : dias 
+    }
+    
+    let date_start= new Date(Date.now())
+    let date_end= new Date(Date.now())
+
+    //tiene fecha de inicio y de fin
+    if (fechaInicio && fechaFin){
+        date_start = fechaInicio
+        if (fecha_hora_inicio !== '' && fechaInicio){
+            date_start = fecha_hora_inicio
+        }
+
+        date_end = fechaFin
+        if (fecha_hora_fin !== '' && fechaFin){
+            date_end = fecha_hora_fin
+        }
+
+        //primero se inicia cuando se indica
+        const job0 = schedule.scheduleJob(date_start,function() {
+            console.log(new Date(Date.now()),"envia primero:",new_msg)
+            publishMessage(pantalla, new_msg);
+        })
+
+        //este sirve para setear cada dia el mensaje
+        const job1 = schedule.scheduleJob({start:date_start,end:date_end,rule:`* 8 * * * ${dias_fin}`},function() {
+            console.log(new Date(Date.now()),"envia normal:",new_msg)
+            publishMessage(pantalla, new_msg);
+        })
+
+        //este sirve para indicar cuando se debe volver al mensaje por defecto
+        const job2 = schedule.scheduleJob(date_end,function() {
+            console.log(new Date(Date.now()),"setea por default:",mensaje_actual)
+            publishMessage(pantalla, new_msg);
+        })
+    }
+    //tiene fecha de inicio y no fin -> es un mensaje por defecto programado
+    else if (fechaInicio && !fechaFin){
+        date_start = fechaInicio
+        if (fecha_hora_inicio !== '' && fechaInicio){
+            date_start = fechaInicio
+        }
+
+        console.log("tiene fecha de inicio y no fin")
+        console.log("date_start:",date_start)
+        //primero se inicia cuando se indica
+        const job0 = schedule.scheduleJob({start:date_start},function() {
+            console.log(new Date(Date.now()),"envia:",new_msg)
+            publishMessage(pantalla, new_msg);
+        })
+
+        //este sirve para setear cada dia el mensaje
+        const job1 = schedule.scheduleJob({start:date_start,rule:`0 8 * * ${dias_fin}`},function() {
+            console.log(new Date(Date.now()),"envia:",new_msg)
+            publishMessage(pantalla, new_msg);
+        })
+    }
+    //no tiene fecha de inicio y menos fin -> es mensaje por defecto
+    else {
+        publishMessage(pantalla, new_msg)
     }
 
-    if (fechaFin) {
-        console.log(`Configuración de la tarea para borrar el mensaje en la fecha de fin: ${fechaFin}`);
-        
-        schedule.scheduleJob(`${jobId}/borrar`, fechaFin, () => {
-        console.log(`Ejecutando la tarea de borrar el mensaje con jobId: ${jobId}/borrar`);
-        // Logic to delete the scheduled message
-        schedule.cancelJob(jobId);
-        });
-    }
+    //FALTARIA VER SI POR EJEMPLO SE TIENE MENSAJE PROGRAMADO LUNES Y MIERCOLES, DEBERIA MARTES,JUEVES....
+    //CREARSE UN CRON PARA VOLVER A MANDAR UN MENSAJE QUE ESTÁ POR DEFECTO
+};
+
+export const scheduleMessageConTiempo = (jobId:any, dias:any, fechaInicio:any, fecha_hora_inicio:any,
+    pantalla:any, mensaje:any, animacion:any,tiempo:number,mensaje_actual:string) => {
+
+    const new_msg = mensaje+"&"+String(animacion)
+    const inicio_msg = fecha_hora_inicio
+    const fin_msg = new Date(inicio_msg.getTime()+(1000*tiempo))
+    const job1 = schedule.scheduleJob(fecha_hora_inicio,function() {
+        console.log(new Date(Date.now()),"envia:",new_msg)
+        publishMessage(pantalla, new_msg);
+    })
+    const job2 = schedule.scheduleJob(fin_msg,function() {
+        console.log(new Date(Date.now()),"setea por default:",mensaje_actual)
+        publishMessage(pantalla, mensaje_actual);
+    })
 };
 
 export const deleteScheduledMessage = (jobId:any) => {
