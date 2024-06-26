399e59082d9e9353d232a25e2761ded81ed8d468
Bastián Núñez <bastinuez@hotmail.com>
2024-06-26 11:51:35 -0400

    feat: console.log para ver fecha

--- utils/mqtt.ts
+++ utils/mqtt.ts
@@ -57,6 +57,7 @@ export const scheduleMessage = async (dias:string, fechaInicio:any, fecha_hora_i
         
         let job0:any;
         const id_job0 = uuidv4()
+        console.log("Hora de inicio que llega a mqtt: ",date_start)
         if (!flag_hora_inicio || date_start < new Date(Date.now())){
             job0 = schedule.scheduleJob(id_job0,new Date(Date.now() + 1000),async function() {
                 //cuando llegue el momento, el mensaje actual cambiará
